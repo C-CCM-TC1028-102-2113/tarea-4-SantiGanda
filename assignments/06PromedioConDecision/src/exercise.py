5f0ae4c776bda60a04d07f8d5d6033b7ed8a6e54
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-09-03 13:14:33 -0500

    Te da el promedio hasta que pongas un número negativo

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,19 @@
 def main():
     #escribe tu código abajo de esta línea
+num=0
+divisor=0
+suma=0
+
+while True:
+    num=float(input())
+    if num>-1:
+        suma=suma+num
+        divisor=divisor+1
+    elif num<=-1:
+        break
+
+print(suma/divisor)
+
     pass
 if __name__=='__main__':
     main()
