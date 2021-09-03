74c7de8c52ac2b576960e4e749e24410ad58fc05
SantiGanda <89093505+SantiGanda@users.noreply.github.com>
2021-09-03 13:23:30 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
+num=int(input())
+carct=1
+simb=1
+a=1
+
+while a<=num:
+    if simb%2!=0:
+        value='#'
+    elif simb%2==0:
+        value='%'
+    print(str(carct)+'-'+str(value))
+    carct=carct+1
+    simb=simb+1
+    a=a+1
+
     pass
 
 if __name__=='__main__':   
